80f197353045ff00f8d06e02e7d7ab8dcf69c4ca
Jun Wang <wangjun@yvr.cn>
2023-05-08 17:42:50 +0800

    so3 fix pose ready

--- matlab/read_paper/so3_test.md
+++ matlab/read_paper/so3_test.md
@@ -14,3 +14,53 @@ $$J = \frac{\delta f}{\delta p} = \frac{M(p+\delta p) -y - (M*p - y )}{\delta p}
 
 ### 2. test fix pose
 
+令真值pose为$$\bar{M}$$, 那么目标函数可以写为：
+
+$$f = ( M \ominus \bar{M}) = (\bar{M}^{-1}*M) = (Y * M)$$
+
+以上$$f\in \mathbb{R}^{3}$$.  那它的雅克比，应该是$$J\in \mathbb{R}^{3\times 3}$$, 参考 eq. 64 (这与SE2的形式都是一样的，且推导过程一样)
+
+$$J^{Y*M}_M = \frac{Log(Y*M*Exp(\tau)\ominus (Y*M))}{\tau}=\frac{Log(M^{-1}Y^{-1}*Y*M*Exp(\tau))}{\tau}=\frac{Log(Exp(\tau))}{\tau} = I$$
+
+与SO2的一样
+
+![image-20230508173040658](/home/junwangcas/Documents/working/typora_imgs/2023/so3_test/image-20230508173040658.png)
+
+---
+
+还有一种情况，当把目标函数定义为如下：
+
+$$f = (\bar{M}\ominus M) = (M^{-1}*\bar{M}) = (X * \bar{M})$$
+
+根据eq. 62.
+
+$$J_M^{M^{-1} * \bar{M}} = J_{X}^{X*\bar{M}} * J_M^{M^{-1}}$$
+
+那就是分别对这两个东西求偏导，第一个公式参考eq. 65。 这里面有一步关键的变换，在eq. 20. 
+
+$$J_{X}^{X*\bar{M}} = (Ad_\bar{M})^{-1}$$
+
+$$J_M^{M^{-1}} = -Ad_M$$
+
+第二个式子，见公式62.  最后，就落脚到求Adm上了
+
+---
+
+SO3的伴随矩阵
+
+关于$$Ad_M$$的求法，下面以SO2为例进行推导(参考Ex 6)
+
+$$M = R, \tau^{\hat{}} = \theta_{\times}, \tau =  \theta$$
+
+$$\bold{Ad}_M * \tau = (M*\tau^{\hat{}}*M^{-1})^{\vee} = (R * \theta_{\times} *  R^T )^{\vee} \\ = (R\theta)_\times$$
+
+这样的话，**带入实际值应用没问题，从形式上这个地方似乎不太保险**。
+
+$$\bold{Ad}_M = R $$
+
+---
+
+![image-20230508173722711](/home/junwangcas/Documents/working/typora_imgs/2023/so3_test/image-20230508173722711.png)
+
+### 3. test poses
+
---
+++ matlab/test_so3/test_fix_pose2.m
@@ -0,0 +1,101 @@
+% landmark
+clear;
+DoF = 3;
+
+NUM_POSES = 1;
+NUM_STATES = NUM_POSES * DoF;
+NUM_MEAS = NUM_POSES * DoF;
+MAX_ITER = 20;
+
+% Simulator
+% poses, controls
+poses_simu = cell(NUM_POSES, 1);
+poses = cell(NUM_POSES, 1);
+
+poses_simu{1} = SO3.eul(0.5, 0.0, 0.0);
+poses{1} = poses_simu{1} * SO3.rand;
+
+%% visualize
+% ground truth 
+figure(1);
+for i = 1:NUM_POSES
+    hold on; poses_simu{i}.plot;
+end
+%xlim([-1, 4]); ylim([-2, 3]);
+axis equal
+title('1 poses - gt');
+xlabel('x'); ylabel('y');zlabel('z');
+% initial values
+figure(2);
+for i = 1:NUM_POSES
+    hold on; poses{i}.plot;
+end
+%xlim([-1, 4]); ylim([-2, 3]);
+%axis equal
+axis equal
+xlabel('x'); ylabel('y');zlabel('z');
+title('2 poses - init');
+
+
+% estimator
+update_ratio = 0.5;
+for iteration = 1:MAX_ITER
+    figure(3);
+    clf;
+    r = zeros(NUM_MEAS, 1);
+    J = zeros(NUM_MEAS, NUM_STATES);
+    row = 1;
+    
+    delta_pose = poses{1}.inv * poses_simu{1};
+    r(row:(row + 2), 1) = so3vec(delta_pose.log);
+    
+    cols = 1:3;
+    J(row:(row+2), cols) = - poses_simu{1}.R' * poses{1}.R; 
+    
+    dx = - inv(J' * J) * J' * r
+    % update
+    poses{1} = poses{1} * SO3.exp(skew([dx()]*update_ratio));
+    
+    % plot
+    %clf;
+    for i = 1:NUM_POSES
+        hold on; poses{i}.plot;
+    end
+    %xlim([-1, 4]); ylim([-2, 3]);
+    %axis equal
+    axis equal
+    title('3 poses - optimization');
+    xlabel('x'); ylabel('y');zlabel('z');
+    pause(0.5);
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
